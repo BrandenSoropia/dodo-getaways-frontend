d04b28a85b3a656e5ac40f0b368a2c3f76d53b93
BrandenSoropia <b.soropia@gmail.com>
2020-05-26 12:05:32 -0400

    Basic HeroSection. Moved Navigation to common. Updated eslint to ignore imports from ui-kit and common.

---
+++ src/HeroSection/HeroSection.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import HeroSection from "./index";
+
+export default {
+  title: `HeroSection`,
+  component: HeroSection,
+};
+
+export const DesktopView = () => <HeroSection />;
---
+++ src/HeroSection/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "ui-kit";
+import Navigation from "common/Navigation";
+
+const HeroSection = () => {
+  return (
+    <Box>
+      <Navigation />
+    </Box>
+  );
+};
+
+export default HeroSection;
